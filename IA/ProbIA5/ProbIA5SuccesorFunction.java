3dbc179e731d551b030d7c225901992d91c71b4e
Guillermo <guillermomed1203@gmail.com>
2023-10-14 17:36:51 +0200

    Added template of project

---
+++ IA/ProbIA5/ProbIA5SuccesorFunction.java
@@ -0,0 +1,26 @@
+package IA.ProbIA5;
+
+import aima.search.framework.SuccessorFunction;
+import aima.search.framework.Successor;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by bejar on 17/01/17
+ */
+public class ProbIA5SuccesorFunction implements SuccessorFunction{
+
+    public List getSuccessors(Object state){
+        ArrayList retval = new ArrayList();
+        ProbIA5Board board = (ProbIA5Board) state;
+
+        // Some code here
+        // (flip all the consecutive pairs of coins and generate new states
+        // Add the states to retval as Succesor("flip i j", new_state)
+        // new_state has to be a copy of state
+
+        return retval;
+
+    }
+
+}
