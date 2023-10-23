b6ec074b8fbd8471f473b8c53b3911c8c7e12edd
SMP03 <sergimp2003@gmail.com>
2023-10-23 22:53:30 +0200

    Completed r script

--- Experiment1.R
+++ Experiment1.R
@@ -1,4 +1,4 @@
-base = "exp2"
+base = "10map50rep" #Posar el nom base especificat a l'script
 
 set1 <- read.table(paste(base, "set1.txt", sep="_"), header=TRUE, sep="\t")
 set2 <- read.table(paste(base, "set2.txt", sep="_"), header=TRUE, sep="\t")
@@ -11,4 +11,12 @@ set3["OpSet"] <- rep(3, nrow(set3))
 sets = rbind(set1,set2,set3)
 b <- boxplot(sets$ExecutionTime ~ sets$OpSet)
 b <- boxplot(sets$TotalProfit ~ sets$OpSet)
+
+# t.tests comprovant si són iguals els profits
+t.test(set1$TotalProfit, set2$TotalProfit, paired = T) # p molt petita i IC lluny de 0 -> set2 millor que set1
+t.test(set2$TotalProfit, set3$TotalProfit, paired = T) # p no tan petita i IC conté 0 -> set2 igual que set3
+
+# t.tests comprovant si són iguals els profits
+t.test(set1$ExecutionTime, set2$ExecutionTime, paired = T) # p molt petita i IC lluny de 0 -> set2 millor que set1
+t.test(set2$ExecutionTime, set3$ExecutionTime, paired = T) # p gran i IC centrat a 0 -> set2 igual que set3
 
