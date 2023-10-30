47b1f62c5190b75cea435deda384cc3080945357
SMP03 <sergimp2003@gmail.com>
2023-10-30 12:34:09 +0100

    Added script for generating Exp4 graphs

--- Experimento4.R
+++ Experimento4.R
@@ -1,16 +1,20 @@
 # Script experimento 4
 base = "test" #Posar el nom base especificat a l'script
+# Omplir amb els paràmetres de l'execució
 min = 25
-max = 200
+max = 250
 step = 25
+reps = 10
 
-expData <- read.table(paste(base, "_stations.txt", sep=""), header=FALSE, sep="\t")
-HCprofits <- read.table(paste(base, "_HC_trace.txt", sep=""), header=FALSE, sep="\t")
+expData <- read.table(paste(base, "_station.txt", sep=""), header=TRUE, sep="\t")
 
-HCMaxProfit <- HCprofits[which.max(HCprofits$V1),]
+expData["NumStations"] = rep(seq(from=min, to=max, by=step), each = 10)
 
-expData <- data.frame(c(0:(nrow(SAprofits)-1)), SAprofits)
-colnames(expData) = noms
+means = c()
 
-ymin = min(HCprofits[which.min(HCprofits$V1),], SAprofits[which.min(SAprofits$V1),])
-ymax = max(HCprofits[which.max(HCprofits$V1),], SAprofits[which.max(SAprofits$V1),])
+for (i in seq(from=min, to=max, by=step)) {
+  means <- append(means, mean(expData$ExecutionTime[expData$NumStations==i]))
+}
+
+plot(y=means, x=seq(from=min, to=max, by=step),
+     xlab="Número de estaciones", ylab="Tiempo de ejecución (ms)")
